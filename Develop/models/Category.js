52c968bf3e86050d6950fe87de46bfb918dc43bf
mayachakra <mayajchakravarty@gmail.com>
2024-04-11 22:45:38 -0400

    fixed bugs through the post method

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product');
+
 
 class Category extends Model {}
 
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try{
@@ -16,56 +16,39 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  try{
-    const categoryData = await Category.findByPk(req.params.id,{
-      include: [{model: Product}],
-    });
-    if(!categoryData){
-      res.status(404).json({ message: 'Not a category in our system'});
-      return;
-    }
-    res.status(200).json(categoryData);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   try{
-    const categoryData = await Category.create({
-      reader_id: req.body.reader_id,
-    });
+    const {category_name} = req.body;
+    const categoryData = await Category.create({category_name});
     res.status(200).json(categoryData);
   }catch(err){
     res.status(500).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const categoryData = await Category.findByPk(req.params.id,{
-      include: [{model: Category}],
+    const categoryData = await Category.update(req.body,{
+      where: {
+        id: req.params.id,
+      },
+      include: [{ model: Product }],
     });
     if(!categoryData){
       res.status(404).json({ message: 'Not a category in our system'});
       return;
     }
     //update category
-    if(req.body.category_name){
-      await categoryData.update({ category_name: req.body.category_name});
-    }
     res.status(200).json(categoryData);
   }catch(err){
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try{
     const categoryData = await Category.destroy({
